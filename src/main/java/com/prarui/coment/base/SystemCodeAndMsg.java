0280b9542e9647af16e978af5821c0b03595ea05
rui <1807520106@qq.com>
2019-08-09 17:23:29 +0800

    token的生成机制

--- src/main/java/com/prarui/coment/base/SystemCodeAndMsg.java
+++ src/main/java/com/prarui/coment/base/SystemCodeAndMsg.java
@@ -1,6 +1,13 @@
 package com.prarui.coment.base;
 
 public class SystemCodeAndMsg {
+    /**
+     * 请求code
+     */
+    public static final int LOGIN_ERROR_CODE=111;
+    /**
+     * message
+     */
     private String msg;
     private String code;
 
